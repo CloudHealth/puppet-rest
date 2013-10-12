33f932eaf54547762a5edf3fd9af7c7c6a7c650a
Steve Frank <lardcanoe@gmail.com>
2013-10-11 21:29:00 -0400

    Expose more attributes of the catalog

--- lib/puppet-rest/pe/entities/catalog.rb
+++ lib/puppet-rest/pe/entities/catalog.rb
@@ -1,9 +1,16 @@
 module PuppetRestClient::PE
   class Catalog < PuppetRestClient::PE::Base
-    attr_reader :name
+    attr_accessor :document_type, :metadata, :tags, :name, :version, :environment, :resources, :edges, :classes
 
     def initialize(attrs=Mash.new)
       super
+      @tags = attrs['data']['tags']
+      @name = attrs['data']['name']
+      @version = attrs['data']['version']
+      @environment = attrs['data']['environment']
+      @resources = attrs['data']['resources']
+      @edges = attrs['data']['edges']
+      @classes = attrs['data']['classes']
     end
   end
 end
